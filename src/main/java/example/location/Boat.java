31adb319bc16e694fef2add4d2193cf4772caf9c
Volant <flavienvolant@gmail.com>
2025-09-26 13:38:20 +0200

    boatTest::embarkAnItemSuccedTest() pass

--- src/main/java/example/location/Boat.java
+++ src/main/java/example/location/Boat.java
@@ -1,21 +1,30 @@
 package example.location;
 
+import example.entity.Item;
 import example.entity.You;
 
 public class Boat{
 
     private You you;
+    private Item item;
 
     public void embark(You you) {
         this.you = you;
+    }
+    
+    public void embark(Item item) {
+        this.item = item;
     }
 
     public Boolean contains(You you) {
         return this.you.equals(you);
     }
+    
+    public Boolean contains(Item item) {
+        return this.item.equals(item);
+    }
 
     public Boolean hasCaptain() {
         return this.you != null;
     }
-
 }
