f22c86b2902dc86920ba2f5654ab2cf80ed4be96
Volant <flavienvolant@gmail.com>
2025-09-28 14:32:30 +0200

    refactor list<item> to list of <entity>

--- src/main/java/example/location/Coast.java
+++ src/main/java/example/location/Coast.java
@@ -3,32 +3,32 @@ package example.location;
 import java.util.ArrayList;
 import java.util.List;
 
-import example.entity.Item;
+import example.entity.Entity;
 
 public class Coast {
-    private List<Item> items;
+    private List<Entity> entities;
 
     public Coast() {
-        this.items = new ArrayList<>();
+        this.entities = new ArrayList<>();
     }
 
     public Boolean isEmpty() {
-        return items.isEmpty();
+        return entities.isEmpty();
     }
 
-    public List<Item> getItems() {
-        return items;
+    public List<Entity> getEntities() {
+        return entities;
     }
 
-    public void add(Item item) {
-        this.items.add(item);
+    public void add(Entity entity) {
+        this.entities.add(entity);
     }
 
-    public boolean contains(Item item) {
-        return this.items.contains(item);
+    public boolean contains(Entity entity) {
+        return this.entities.contains(entity);
     }
 
-    public void remove(Item item) {
-        this.items.remove(item);
+    public void remove(Entity entity) {
+        this.entities.remove(entity);
     }
 }
